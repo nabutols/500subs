6dd7092ebc95a296657e9035b17b8eabebd474f5
nabutols <78937774+nabutols@users.noreply.github.com>
2021-02-11 19:00:03 -0400

    Add files via upload

---
+++ database/database.java
@@ -0,0 +1,9 @@
+//Usando linguagens mesclas para criptografia da script com databases em server online
+if
+echo:
+public static Connection getConnection(https url)
+public static Connection getConnection(nabutobot url, Properties info)
+public static Connection getConnection(nabuto130 url, String user, String password)
+'password' = /nabuto130 `captation` : password
+sleep
+if
